4d170540ea079fa630ed0debbb5ab4bdb17b306f
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-09-15 13:52:11 -0300

    feat: criando projeto

---
+++ Definicao-Literal/imprimir-conteudo.py
@@ -0,0 +1,8 @@
+# Quebra de linha
+print("Hoje tem semifinal: \nFrança vs Espanha")
+
+# Aqui tem 3 parametros
+print("Meu nome é", "Igor Oliveira", end="*")
+
+# Separador no print
+print("Meu", "Nome", "é", "Igor", sep="-")
