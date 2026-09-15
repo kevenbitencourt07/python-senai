3c62875ed9dabdec1f0e14d21c55c7c2b224ea47
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-09-15 14:29:48 -0300

    feat: add

---
+++ Definicao-Literal/literais.py
@@ -0,0 +1,15 @@
+# String
+nome = "Keven"
+
+# Números inteiros e decimais
+idade = 18
+altura = 1.72
+
+# Valor lógico (True e False)
+professor = False
+
+# Verifica a tipagem das variáveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
