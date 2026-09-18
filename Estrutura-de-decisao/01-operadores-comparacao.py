42301f9632fc0ab4223c575b0b76d213b3da7127
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-09-18 14:11:38 -0300

    feat: add

---
+++ Estrutura-de-decisao/01-operadores-comparacao.py
@@ -0,0 +1,23 @@
+# Entrada de dados
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+# Exibe o resultado das expressões
+
+# Verifica se os números são iguais
+print("num1 == num2: ", num1 == num2)
+
+# Verifica se os números são diferentes
+print("num1 != num2: ", num1 != num2)
+
+# Verifica se num1 é menor que o num2
+print("num1 < num2: ", num1 < num2)
+
+# Verifica se num1 é menor ou igual que o num2
+print("num1 <= num2: ", num1 <= num2)
+
+# Verifica se num1 é maior que o num2
+print("num1 > num2: ", num1 > num2)
+
+# Verifica se num1 é maior ou igual que o num2
+print("num1 >= num2: ", num1 >= num2)
