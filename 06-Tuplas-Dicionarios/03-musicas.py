03f668c04ecdcb3195c777a2fdc56ca54273ae18
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-09-25 14:01:30 -0300

    feat: add tuplas-dicionarios

---
+++ 06-Tuplas-Dicionarios/03-musicas.py
@@ -0,0 +1,8 @@
+musicas = ("Evidências", "Pais e Filhos", "Tempo Perdido", "Anna Júlia")
+
+# Percorrendo a tupla
+for musica in musicas:
+    if musica == "Evidências":
+        print("Encontrei: ", musica)
+    else:
+        print("Música:", musica)
---
+++ 06-Tuplas-Dicionarios/04-exercicio-cursos.py
@@ -0,0 +1,6 @@
+# Enunciado:
+# Crie uma tupla com 5 cursos. 
+# Use for e if para verificar se o curso "Python" está presente.
+
+# Tupla com cursos
+cursos = ("Python", "Java", "HTML", "JavaScript", "C++")
